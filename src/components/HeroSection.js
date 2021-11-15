196f8b925c4c9e702459165d6108a6c1423c85af
Gerhard <gerhard.putter@yahoo.com>
2021-11-15 10:23:40 +0200

    Added Hero section and styles.

---
+++ src/components/HeroSection.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './HeroSection.css';
+import { Button } from './Button'
+
+function HeroSection() {
+    return (
+        <div className='hero-container'>
+            <video src="./video/video-2.mp4" type="video/mp4" autoPlay loop muted />
+            <h1>SSPA</h1>
+            <p>It all adds up.</p>
+            <div className="hero-btns">
+                <Button className="btns" buttonStyle="btn--outline" buttonSize="btn--large">Contact Me</Button>
+                <Button className="btns" buttonStyle="btn--primary" buttonSize="btn--large">Need Help <i className="far fa-play-circle" /></Button>
+            </div>
+
+        </div>
+    )
+}
+
+export default HeroSection
