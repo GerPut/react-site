bcc2eca57a887ef8e281a6c24ee71f8ef89e3d9a
Gerhard <gerhard.putter@yahoo.com>
2021-11-21 19:12:49 +0200

    Updated.

--- src/components/About.js
+++ src/components/About.js
@@ -7,8 +7,8 @@ function About() {
             <section>
                 <h1>About Us</h1>
                 <div className="about-us-container">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil illum doloremque a sapiente, maxime aspernatur praesentium consequatur in eos tempora ut numquam! Voluptate, ipsa officiis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis dolore repudiandae perspiciatis voluptas sint cumque, id aspernatur iste aut perferendis voluptatibus ipsam praesentium dignissimos distinctio quam quos maxime alias. Laborum vel, architecto, minus tenetur dolorum officiis laboriosam, assumenda optio cupiditate minima non! Eaque voluptates, maiores quaerat voluptatem consectetur aperiam libero.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil illum doloremque a sapiente, maxime aspernatur praesentium consequatur in eos tempora ut numquam! Voluptate, ipsa officiis!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil illum doloremque a sapiente, maxime aspernatur praesentium consequatur in eos tempora ut numquam! Voluptate, ipsa officiis! Lorem ipsum dolor sit am Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem soluta tempora, similique sint possimus culpa alias aperiam, molestiae inventore cumque laudantium corrupti numquam nulla odit.</p>
+
                 </div>
             </section>
         </div>
