3563dca66a16796c1e7f4a490b26b1581de69191
Gerhard <gerhard.putter@yahoo.com>
2021-11-21 19:09:48 +0200

    Added Services section.

---
+++ src/components/Services.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Services.css'
+
+function Services() {
+    return (
+        <div className="services-main">
+            <section id="services">
+                <h1>Services</h1>
+                <div className="services-container">
+                    <div>
+                        <p>Individual Tax & Tax Advice</p>
+                        <p> CIPC Secretarial Services</p>
+                        <p>Monthly Accounting & VAT</p>
+                        <p>COID & Return of Earning</p>
+                        <p>Letter of Good Standing</p>
+                        <p>Company Registrations</p>
+                        <p>Financial Statements</p>
+                        <p>Payroll & UIF</p>
+                        <p>Entity Tax</p>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Services;
