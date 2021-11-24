4491092288592528bcf2261df9060d1efa2acafe
Gerhard <gerhard.putter@yahoo.com>
2021-11-24 17:37:49 +0200

    Styling of page.

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -3,6 +3,21 @@ import '../../App.css'
 
 export default function Services() {
     return (
-        <h1 className="services">Services</h1>
+        <div className="services">
+            <h1 >Services</h1>
+            <div className="services-container">
+                <p>Hello</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, sint dolorum labore neque laudantium sequi beatae amet et consectetur temporibus.</p>
+                <ul>
+                    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, quam?</li>
+                    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, quam?</li>
+                    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, quam?</li>
+                    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, quam?</li>
+                    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, quam?</li>
+                </ul>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum recusandae, corporis reprehenderit, alias ut at quasi maiores facere facilis non eveniet voluptas sapiente iure a?
+                </p>
+            </div>
+        </div>
     )
 }
