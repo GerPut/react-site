322dd6764cce9c995aa7d08fe8aff4c627af16fc
Gerhard <gerhard.putter@yahoo.com>
2021-11-27 15:03:01 +0200

    Updated.

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -6,15 +6,9 @@ export default function AboutPage() {
         <div className="services">
             <h1>About</h1>
             <div className="about-page-container">
-                <p>about</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, sint dolorum labore neque laudantium sequi beatae amet et consectetur temporibus.</p>
-                <ul>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, quam?</li>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, quam?</li>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, quam?</li>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, quam?</li>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, quam?</li>
-                </ul>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum recusandae, corporis reprehenderit, alias ut at quasi maiores facere facilis non eveniet voluptas sapiente iure a?
+                </p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum recusandae, corporis reprehenderit, alias ut at quasi maiores facere facilis non eveniet voluptas sapiente iure a?
                 </p>
             </div>
--- src/components/pages/ContactUs.js
+++ src/components/pages/ContactUs.js
@@ -6,10 +6,12 @@ export default function ContactUs() {
         <div className="contact">
             <h1>Contact</h1>
             <div className="contact-us-page-container">
-                <p>Office Hours:</p>
+                <p><strong>Business Hours:</strong></p>
                 <p>Mon-Fri: 8:00-17:00</p>
+                <p>Saturdays & Sundays: Closed</p>
+                <p><strong>Telephone:</strong></p>
                 <p><i class="fas fa-phone-square"></i> +2700 000 0000</p>
-
+                <p><strong>E-mail:</strong></p>
                 <p><i class="fas fa-envelope"></i> rtor@titiri.co.za
                 </p>
             </div>
